f9161f8830d0096f6cd69048363bc8200e92b7a6
Jorge Eyzaguirre <jorgeyza@gmail.com>
2022-03-06 15:01:41 -0500

    Add Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,29 @@
-import { Box, Flex } from '@chakra-ui/react';
-import { ChakraNextImage } from 'utils/ChakraNextImage';
+import { Flex, Text } from '@chakra-ui/react';
+import CustomLink from 'components/CustomLink';
+import { LogoIcon, LogoText } from 'components/Logo';
 
-const Navbar = () => {
-  const lala = '';
-
-  return <Flex>something</Flex>;
-};
+const Navbar = () => (
+  <Flex backgroundColor='yellow.100' color='gray.700' justifyContent='space-between' paddingX={4} paddingY={2}>
+    <Flex alignItems='center' columnGap={4}>
+      <CustomLink url='/'>
+        <Flex alignItems='center' columnGap={4}>
+          <LogoIcon />
+          <LogoText />
+        </Flex>
+      </CustomLink>
+      <CustomLink url='#'>
+        <Text>Submit a link</Text>
+      </CustomLink>
+    </Flex>
+    <Flex alignItems='center' columnGap={4}>
+      <CustomLink url='/login'>
+        <Text>Login</Text>
+      </CustomLink>
+      <CustomLink url='/register'>
+        <Text>Register</Text>
+      </CustomLink>
+    </Flex>
+  </Flex>
+);
 
 export default Navbar;
