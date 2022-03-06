131052994f47be97b090ff84aa821d7597dfacb0
Jorge Eyzaguirre <jorgeyza@gmail.com>
2022-03-06 14:59:19 -0500

    Update page loading AND Add Layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,43 @@
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { ChakraProvider } from '@chakra-ui/react';
+import NProgress from 'nprogress';
 
-import PageLoader from 'components/PageLoader';
+import Layout from 'components/Layout';
 
 import { theme } from 'theme';
 
 import '@fontsource/open-sans/400.css';
 import '@fontsource/open-sans/500.css';
 import '@fontsource/open-sans/700.css';
+import 'nprogress/nprogress.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    router.events.on('routeChangeStart', handleRouteStart);
+    router.events.on('routeChangeComplete', handleRouteDone);
+    router.events.on('routeChangeError', handleRouteDone);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteStart);
+      router.events.off('routeChangeComplete', handleRouteDone);
+      router.events.off('routeChangeError', handleRouteDone);
+    };
+  }, [router.events]);
+
   return (
     <ChakraProvider theme={theme}>
-      <PageLoader />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
-}
+};
 
 export default MyApp;
