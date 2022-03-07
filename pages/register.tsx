c4086b30a02e4ddaa827ee59bbac6b8b4aa558ac
Jorge Eyzaguirre <jorgeyza@gmail.com>
2022-03-06 19:34:50 -0500

    Update Register page

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,6 +1,12 @@
+import { Container, Heading } from '@chakra-ui/react';
 import Form from 'components/Form';
 import type { NextPage } from 'next';
 
-const Register: NextPage = () => <Form />;
+const Register: NextPage = () => (
+  <Container maxW='container.md'>
+    <Heading as='h1'>Register</Heading>
+    <Form />
+  </Container>
+);
 
 export default Register;
